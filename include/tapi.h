02795785f7c2e193f6321a1687cc4d3ce58cb58a
yuexianghui <yuexianghui@xiaomi.com>
2023-03-03 10:37:41 +0800

    ofono/telephony: enable/disable modem & get modem status tapi
    Signed-off-by: yuexianghui <yuexianghui@xiaomi.com>

--- include/tapi.h
+++ include/tapi.h
@@ -161,6 +161,7 @@ typedef enum {
     MSG_RADIO_STATE_CHANGE_IND = 0,
     MSG_PHONE_STATE_CHANGE_IND,
     MSG_OEM_HOOK_RAW_IND,
+    MSG_MODEM_RESTART_IND,
 
     // Call Indication Message
     MSG_CALL_ADD_MESSAGE_IND,
