ce91046274d4fa586b02fd229b49e59a26666e89
fanguoxiang <fanguoxiang@xiaomi.com>
2023-03-05 15:25:59 +0800

    frameworks/telephony: add query/insert/update/del fdn record tapi
    Change-Id: Icb6e241b4fefaa29372a3a92b4ced5c2b3065308

--- include/tapi_phonebook.h
+++ include/tapi_phonebook.h
@@ -28,6 +28,22 @@
 #include "tapi.h"
 
 /****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define MAX_FDN_RECORD_LENGTH 10
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+typedef struct {
+    int fdn_idx; // record numer in EFFDN
+    char* tag;
+    char* number;
+} fdn_entry;
+
+/****************************************************************************
  * Public Function Prototypes
  ****************************************************************************/
 
@@ -45,6 +61,61 @@ extern "C" {
  */
 int tapi_phonebook_load_adn_entries(tapi_context context,
     int slot_id, int event_id, tapi_async_function p_handle);
+
+/**
+ * Query the FDN records in the SIM phonebook.
+ * @param[in] context        Telephony api context.
+ * @param[in] slot_id        Slot id of current sim.
+ * @param[in] event_id       Async event identifier.
+ * @param[in] p_handle       Event callback.
+ * @return Positive value as watch_id on success; a negated errno value on failure.
+ */
+int tapi_phonebook_load_fdn_entries(tapi_context context,
+    int slot_id, int event_id, tapi_async_function p_handle);
+
+/**
+ * Insert the FDN record into the SIM phonebook.
+ * @param[in] context        Telephony api context.
+ * @param[in] slot_id        Slot id of current sim.
+ * @param[in] event_id       Async event identifier.
+ * @param[in] name           FDN entry tag.
+ * @param[in] number         FDN entry number.
+ * @param[in] pin2           PIN2 code.
+ * @param[in] p_handle       Event callback.
+ * @return Positive value as watch_id on success; a negated errno value on failure.
+ */
+int tapi_phonebook_insert_fdn_entry(tapi_context context,
+    int slot_id, int event_id, char* name, char* number,
+    char* pin2, tapi_async_function p_handle);
+
+/**
+ * Delete the FDN record in the SIM phonebook.
+ * @param[in] context        Telephony api context.
+ * @param[in] slot_id        Slot id of current sim.
+ * @param[in] event_id       Async event identifier.
+ * @param[in] fdn_idx        Record number on EFFDN.
+ * @param[in] pin2           PIN2 code.
+ * @param[in] p_handle       Event callback.
+ * @return Positive value as watch_id on success; a negated errno value on failure.
+ */
+int tapi_phonebook_delete_fdn_entry(tapi_context context, int slot_id,
+    int event_id, int fdn_idx, char* pin2, tapi_async_function p_handle);
+
+/**
+ * Update the FDN record to the SIM phonebook.
+ * @param[in] context        Telephony api context.
+ * @param[in] slot_id        Slot id of current sim.
+ * @param[in] event_id       Async event identifier.
+ * @param[in] fdn_idx        Record number on EFFDN.
+ * @param[in] new_name       New Entry name.
+ * @param[in] new_number     New Entry number.
+ * @param[in] pin2           PIN2 code.
+ * @param[in] p_handle       Event callback.
+ * @return Positive value as watch_id on success; a negated errno value on failure.
+ */
+int tapi_phonebook_update_fdn_entry(tapi_context context,
+    int slot_id, int event_id, int fdn_idx, char* new_name,
+    char* new_number, char* pin2, tapi_async_function p_handle);
 
 #ifdef __cplusplus
 }
--- tapi_phonebook.c
+++ tapi_phonebook.c
@@ -27,12 +27,27 @@
  * Private Type Declarations
  ****************************************************************************/
 
+typedef struct {
+    int fdn_idx;
+    char* tag;
+    char* number;
+    char* pin2;
+} fdn_record_param;
+
 /****************************************************************************
  * Private Function Prototypes
  ****************************************************************************/
 
 static void user_data_free(void* user_data);
+/* This method should be called if the data field needs to be recycled. */
+static void user_data_free2(void* user_data);
 static void load_adn_entries_cb(DBusMessage* message, void* user_data);
+static void load_fdn_entries_cb(DBusMessage* message, void* user_data);
+static void insert_fdn_record_append(DBusMessageIter* iter, void* user_data);
+static void insert_fdn_record_cb(DBusMessage* message, void* user_data);
+static void delete_fdn_record_append(DBusMessageIter* iter, void* user_data);
+static void update_fdn_record_append(DBusMessageIter* iter, void* user_data);
+static void method_call_complete(DBusMessage* message, void* user_data);
 
 /****************************************************************************
  * Private Functions
@@ -47,6 +62,27 @@ static void user_data_free(void* user_data)
         ar = handler->result;
         if (ar != NULL)
             free(ar);
+
+        free(handler);
+    }
+}
+
+/* This method should be called if the data field needs to be recycled. */
+static void user_data_free2(void* user_data)
+{
+    tapi_async_handler* handler = user_data;
+    tapi_async_result* ar;
+    void* data;
+
+    if (handler != NULL) {
+        ar = handler->result;
+        if (ar != NULL) {
+            data = ar->data;
+            if (data != NULL)
+                free(data);
+
+            free(ar);
+        }
 
         free(handler);
     }
@@ -84,6 +120,206 @@ static void load_adn_entries_cb(DBusMessage* message, void* user_data)
     dbus_message_iter_get_basic(&iter, &entries);
 
     ar->data = entries;
+    ar->status = OK;
+
+done:
+    cb(ar);
+}
+
+static void load_fdn_entries_cb(DBusMessage* message, void* user_data)
+{
+    fdn_entry entries[MAX_FDN_RECORD_LENGTH];
+    DBusMessageIter iter, arr, entry;
+    tapi_async_handler* handler;
+    tapi_async_result* ar;
+    tapi_async_function cb;
+    DBusError err;
+    int index;
+
+    handler = user_data;
+    if (handler == NULL)
+        return;
+
+    if ((ar = handler->result) == NULL || (cb = handler->cb_function) == NULL)
+        return;
+
+    dbus_error_init(&err);
+    if (dbus_set_error_from_message(&err, message) == true) {
+        tapi_log_error("%s: %s\n", err.name, err.message);
+        dbus_error_free(&err);
+        ar->status = ERROR;
+        goto done;
+    }
+
+    if (dbus_message_iter_init(message, &iter) == false) {
+        ar->status = ERROR;
+        goto done;
+    }
+
+    dbus_message_iter_recurse(&iter, &arr);
+    if (dbus_message_iter_get_arg_type(&arr) == DBUS_TYPE_ARRAY) {
+        dbus_message_iter_recurse(&arr, &entry);
+
+        index = 0;
+        while (dbus_message_iter_get_arg_type(&entry) != DBUS_TYPE_INVALID) {
+            if (dbus_message_iter_get_arg_type(&entry) == DBUS_TYPE_STRUCT) {
+                dbus_message_iter_get_basic(&entry, &entries[index++].fdn_idx);
+                dbus_message_iter_next(&entry);
+
+                dbus_message_iter_get_basic(&entry, &entries[index++].tag);
+                dbus_message_iter_next(&entry);
+
+                dbus_message_iter_get_basic(&entry, &entries[index++].number);
+            }
+
+            if (index >= MAX_FDN_RECORD_LENGTH)
+                break;
+
+            dbus_message_iter_next(&arr);
+            dbus_message_iter_recurse(&arr, &entry);
+        }
+
+        ar->arg2 = index;
+    }
+
+    ar->data = entries;
+    ar->status = OK;
+
+done:
+    cb(ar);
+}
+
+static void insert_fdn_record_append(DBusMessageIter* iter, void* user_data)
+{
+    tapi_async_handler* param = user_data;
+    fdn_record_param* insert_fdn_record;
+    char* name;
+    char* number;
+    char* pin2;
+
+    if (param == NULL || param->result == NULL) {
+        tapi_log_error("invalid pin argument!");
+        return;
+    }
+
+    insert_fdn_record = param->result->data;
+    name = insert_fdn_record->tag;
+    number = insert_fdn_record->number;
+    pin2 = insert_fdn_record->pin2;
+
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING, &name);
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING, &number);
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING, &pin2);
+}
+
+static void insert_fdn_record_cb(DBusMessage* message, void* user_data)
+{
+    DBusMessageIter iter;
+    tapi_async_handler* handler;
+    tapi_async_result* ar;
+    tapi_async_function cb;
+    DBusError err;
+
+    handler = user_data;
+    if (handler == NULL)
+        return;
+
+    if ((ar = handler->result) == NULL || (cb = handler->cb_function) == NULL)
+        return;
+
+    dbus_error_init(&err);
+    if (dbus_set_error_from_message(&err, message) == true) {
+        tapi_log_error("%s: %s\n", err.name, err.message);
+        dbus_error_free(&err);
+        ar->status = ERROR;
+        goto done;
+    }
+
+    if (dbus_message_iter_init(message, &iter) == false) {
+        ar->status = ERROR;
+        goto done;
+    }
+
+    dbus_message_iter_get_basic(&iter, &ar->arg2); /*fdn index*/
+
+    ar->status = OK;
+
+done:
+    cb(ar);
+}
+
+static void delete_fdn_record_append(DBusMessageIter* iter, void* user_data)
+{
+    tapi_async_handler* param = user_data;
+    fdn_record_param* delete_fdn_record;
+    int fdn_idx;
+    char* pin2;
+
+    if (param == NULL || param->result == NULL) {
+        tapi_log_error("invalid pin argument!");
+        return;
+    }
+
+    delete_fdn_record = param->result->data;
+    pin2 = delete_fdn_record->pin2;
+    fdn_idx = delete_fdn_record->fdn_idx;
+
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING, &pin2);
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_INT32, &fdn_idx);
+}
+
+static void update_fdn_record_append(DBusMessageIter* iter, void* user_data)
+{
+    tapi_async_handler* param = user_data;
+    fdn_record_param* update_fdn_record;
+    char* new_number;
+    char* new_name;
+    int fdn_idx;
+    char* pin2;
+
+    if (param == NULL || param->result == NULL) {
+        tapi_log_error("invalid pin argument!");
+        return;
+    }
+
+    update_fdn_record = param->result->data;
+    new_name = update_fdn_record->tag;
+    new_number = update_fdn_record->number;
+    pin2 = update_fdn_record->pin2;
+    fdn_idx = update_fdn_record->fdn_idx;
+
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING, &new_name);
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING, &new_number);
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING, &pin2);
+    dbus_message_iter_append_basic(iter, DBUS_TYPE_INT32, &fdn_idx);
+}
+
+static void method_call_complete(DBusMessage* message, void* user_data)
+{
+    tapi_async_handler* handler = user_data;
+    tapi_async_result* ar;
+    tapi_async_function cb;
+    DBusError err;
+
+    if (handler == NULL)
+        return;
+
+    ar = handler->result;
+    if (ar == NULL)
+        return;
+
+    cb = handler->cb_function;
+    if (cb == NULL)
+        return;
+
+    dbus_error_init(&err);
+    if (dbus_set_error_from_message(&err, message) == true) {
+        tapi_log_error("%s error %s: %s \n", __func__, err.name, err.message);
+        dbus_error_free(&err);
+        ar->status = ERROR;
+        goto done;
+    }
+
     ar->status = OK;
 
 done:
@@ -130,6 +366,211 @@ int tapi_phonebook_load_adn_entries(tapi_context context,
     if (!g_dbus_proxy_method_call(proxy, "Import", NULL,
             load_adn_entries_cb, user_data, user_data_free)) {
         user_data_free(user_data);
+        return -EINVAL;
+    }
+
+    return OK;
+}
+
+int tapi_phonebook_load_fdn_entries(tapi_context context,
+    int slot_id, int event_id, tapi_async_function p_handle)
+{
+    tapi_async_handler* user_data;
+    dbus_context* ctx = context;
+    tapi_async_result* ar;
+    GDBusProxy* proxy;
+
+    if (ctx == NULL || !tapi_is_valid_slotid(slot_id)) {
+        return -EINVAL;
+    }
+
+    proxy = ctx->dbus_proxy[slot_id][DBUS_PROXY_PHONEBOOK];
+    if (proxy == NULL) {
+        tapi_log_error("no available proxy ...\n");
+        return -EIO;
+    }
+
+    ar = malloc(sizeof(tapi_async_result));
+    if (ar == NULL) {
+        return -ENOMEM;
+    }
+    ar->msg_id = event_id;
+    ar->arg1 = slot_id;
+
+    user_data = malloc(sizeof(tapi_async_handler));
+    if (user_data == NULL) {
+        free(ar);
+        return -ENOMEM;
+    }
+    user_data->cb_function = p_handle;
+    user_data->result = ar;
+
+    if (!g_dbus_proxy_method_call(proxy, "ImportFdn", NULL,
+            load_fdn_entries_cb, user_data, user_data_free)) {
+        user_data_free(user_data);
+        return -EINVAL;
+    }
+
+    return OK;
+}
+
+int tapi_phonebook_insert_fdn_entry(tapi_context context, int slot_id,
+    int event_id, char* name, char* number, char* pin2, tapi_async_function p_handle)
+{
+    fdn_record_param* fdn_record;
+    tapi_async_handler* user_data;
+    dbus_context* ctx = context;
+    tapi_async_result* ar;
+    GDBusProxy* proxy;
+
+    if (ctx == NULL || !tapi_is_valid_slotid(slot_id)
+        || name == NULL || number == NULL || pin2 == NULL) {
+        return -EINVAL;
+    }
+
+    proxy = ctx->dbus_proxy[slot_id][DBUS_PROXY_PHONEBOOK];
+    if (proxy == NULL) {
+        tapi_log_error("no available proxy ...\n");
+        return -EIO;
+    }
+
+    fdn_record = malloc(sizeof(fdn_record_param));
+    if (fdn_record == NULL) {
+        return -ENOMEM;
+    }
+    fdn_record->tag = name;
+    fdn_record->number = number;
+    fdn_record->pin2 = pin2;
+
+    ar = malloc(sizeof(tapi_async_result));
+    if (ar == NULL) {
+        return -ENOMEM;
+    }
+    ar->msg_id = event_id;
+    ar->arg1 = slot_id;
+    ar->data = fdn_record;
+
+    user_data = malloc(sizeof(tapi_async_handler));
+    if (user_data == NULL) {
+        free(fdn_record);
+        free(ar);
+        return -ENOMEM;
+    }
+    user_data->cb_function = p_handle;
+    user_data->result = ar;
+
+    if (!g_dbus_proxy_method_call(proxy, "InsertFdn", insert_fdn_record_append,
+            insert_fdn_record_cb, user_data, user_data_free2)) {
+        user_data_free2(user_data);
+        return -EINVAL;
+    }
+
+    return OK;
+}
+
+int tapi_phonebook_delete_fdn_entry(tapi_context context, int slot_id,
+    int event_id, int fdn_idx, char* pin2, tapi_async_function p_handle)
+{
+    fdn_record_param* fdn_record;
+    tapi_async_handler* user_data;
+    dbus_context* ctx = context;
+    tapi_async_result* ar;
+    GDBusProxy* proxy;
+
+    if (ctx == NULL || !tapi_is_valid_slotid(slot_id)
+        || fdn_idx < 1 || pin2 == NULL) {
+        return -EINVAL;
+    }
+
+    proxy = ctx->dbus_proxy[slot_id][DBUS_PROXY_PHONEBOOK];
+    if (proxy == NULL) {
+        tapi_log_error("no available proxy ...\n");
+        return -EIO;
+    }
+
+    fdn_record = malloc(sizeof(fdn_record_param));
+    if (fdn_record == NULL) {
+        return -ENOMEM;
+    }
+    fdn_record->fdn_idx = fdn_idx;
+    fdn_record->pin2 = pin2;
+
+    ar = malloc(sizeof(tapi_async_result));
+    if (ar == NULL) {
+        return -ENOMEM;
+    }
+    ar->msg_id = event_id;
+    ar->arg1 = slot_id;
+    ar->data = fdn_record;
+
+    user_data = malloc(sizeof(tapi_async_handler));
+    if (user_data == NULL) {
+        free(fdn_record);
+        free(ar);
+        return -ENOMEM;
+    }
+    user_data->cb_function = p_handle;
+    user_data->result = ar;
+
+    if (!g_dbus_proxy_method_call(proxy, "DeleteFdn", delete_fdn_record_append,
+            method_call_complete, user_data, user_data_free2)) {
+        user_data_free2(user_data);
+        return -EINVAL;
+    }
+
+    return OK;
+}
+
+int tapi_phonebook_update_fdn_entry(tapi_context context, int slot_id, int event_id,
+    int fdn_idx, char* new_name, char* new_number, char* pin2, tapi_async_function p_handle)
+{
+    tapi_async_handler* user_data;
+    dbus_context* ctx = context;
+    fdn_record_param* fdn_record;
+    tapi_async_result* ar;
+    GDBusProxy* proxy;
+
+    if (ctx == NULL || !tapi_is_valid_slotid(slot_id)
+        || fdn_idx < 1 || new_name == NULL
+        || new_number == NULL || pin2 == NULL) {
+        return -EINVAL;
+    }
+
+    proxy = ctx->dbus_proxy[slot_id][DBUS_PROXY_PHONEBOOK];
+    if (proxy == NULL) {
+        tapi_log_error("no available proxy ...\n");
+        return -EIO;
+    }
+
+    fdn_record = malloc(sizeof(fdn_record_param));
+    if (fdn_record == NULL) {
+        return -ENOMEM;
+    }
+    fdn_record->fdn_idx = fdn_idx;
+    fdn_record->tag = new_name;
+    fdn_record->number = new_number;
+    fdn_record->pin2 = pin2;
+
+    ar = malloc(sizeof(tapi_async_result));
+    if (ar == NULL) {
+        return -ENOMEM;
+    }
+    ar->msg_id = event_id;
+    ar->arg1 = slot_id;
+    ar->data = fdn_record;
+
+    user_data = malloc(sizeof(tapi_async_handler));
+    if (user_data == NULL) {
+        free(fdn_record);
+        free(ar);
+        return -ENOMEM;
+    }
+    user_data->cb_function = p_handle;
+    user_data->result = ar;
+
+    if (!g_dbus_proxy_method_call(proxy, "UpdateFdn", update_fdn_record_append,
+            method_call_complete, user_data, user_data_free2)) {
+        user_data_free2(user_data);
         return -EINVAL;
     }
 
