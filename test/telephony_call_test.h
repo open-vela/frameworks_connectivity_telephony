d0cc6902d6a743f37335518b41b83f59d89ab7e2
yuanliping <yuanliping@xiaomi.com>
2024-12-17 22:02:15 +0800

    frameworks/telephony: add call_incoming_after_remote_hangup case
    Signed-off-by: yuanliping <yuanliping@xiaomi.com>

--- test/telephony_call_test.h
+++ test/telephony_call_test.h
@@ -57,11 +57,9 @@ int call_dial_after_caller_reject(int slot_id);
 int call_dial_number_test(int slot_id);
 int call_dial_with_area_code(int slot_id);
 int call_dial_after_caller_reject(int slot_id);
-int call_hangup_after_caller_answer(int slot_id);
 int outgoing_call_remote_answer_and_hangup(int slot_id);
 int outgoing_call_hold_and_unhold_by_caller(int slot_id);
 int outgoing_call_active_and_send_tones(int slot_id);
-int call_dial_caller_reject_and_incoming(int slot_id);
 int call_dial_caller_reject_and_dial_another(int slot_id);
 int incoming_call_answer_and_remote_hangup(int slot_id);
 int call_dial_to_phone_out_of_service(int slot_id);
@@ -122,4 +120,5 @@ int call_clear_voicecall_slot_set(void);
 int call_connect_and_local_hangup(int slot_id);
 int dial_and_remote_hangup(int slot_id);
 int incoming_call_and_local_hangup(int slot_id);
+int call_incoming_after_remote_hangup(int slot_id);
 #endif
