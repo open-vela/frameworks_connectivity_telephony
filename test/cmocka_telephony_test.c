ad65c14cc49f61eb56efbabdcb0b9e39f1dca309
yuanliping <yuanliping@xiaomi.com>
2024-12-30 15:49:30 +0800

    frameworks/telephony: add call_incoming_hangup_first_answer_call case
    Signed-off-by: yuanliping <yuanliping@xiaomi.com>

--- test/cmocka_telephony_test.c
+++ test/cmocka_telephony_test.c
@@ -605,6 +605,13 @@ static void TestTeleFunc_CallIncomingAndHangupNewCall(void** state)
     assert_int_equal(ret, 0);
 }
 
+static void TestTeleFunc_CallIncomingHangupFirstAnswerCall(void** state)
+{
+    (void)state;
+    int ret = call_incoming_hangup_first_answer_call(0);
+    assert_int_equal(ret, 0);
+}
+
 // static void TestTeleLoadEccList(void** state)
 // {
 //     (void)state;
@@ -2365,6 +2372,7 @@ int main(int argc, char* argv[])
         cmocka_unit_test_setup_teardown(TestTeleFunc_CallIncomingandLocalHangup, setup_call, teardown_call),
         cmocka_unit_test_setup_teardown(TestTeleFunc_CallIncomingAfterRemoteHangup, setup_call, teardown_call),
         cmocka_unit_test_setup_teardown(TestTeleFunc_CallIncomingAndHangupNewCall, setup_call, teardown_call),
+        cmocka_unit_test_setup_teardown(TestTeleFunc_CallIncomingHangupFirstAnswerCall, setup_call, teardown_call),
         cmocka_unit_test_setup_teardown(TestTeleFunc_CallSetVoicecallSlot, setup_call, teardown_call),
         cmocka_unit_test_setup_teardown(TestTeleFunc_CallGetVoicecallSlot, setup_call, teardown_call),
         cmocka_unit_test_setup_teardown(TestTeleFunc_CallClearVoicecallSlot, setup_call, teardown_call),
